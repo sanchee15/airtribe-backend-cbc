7a32de3131e698ce7e2664d0655bad9b5822c365
Sanchee <sanchee.kaushal@gmail.com>
2023-05-08 11:57:08 -0700

    Add code for transactions and locks

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const sequalize = require('../configs/mysqldb').sequalize;
 const Product = require('../models/products');
 
 
@@ -30,7 +31,7 @@ router.post('/products', async(request, response) => {
 router.get('/product/:id', async(request, response) => {
 	const product = Product.findOne({
 		where: {
-			id: request.params.id
+			id: parseInt(request.params.id)
 		}
 	});
 	await product.then(function(success){
@@ -46,7 +47,7 @@ router.patch('/product/:id', async(request, response) => {
 
 	const product = Product.update(data, {
 		where: {
-			id: request.params.id,
+			id: parseInt(request.params.id),
 		}
 	});
 
@@ -56,5 +57,81 @@ router.patch('/product/:id', async(request, response) => {
 		response.json(error);
 	});
 });
+
+
+router.post('/product/:id/stock_update_self_managed', async(request, response) => {
+	// Assuming Product ID and Product ID + 1 represent the same product type
+	const data = request.body;
+	const transaction = await sequalize.transaction()
+	try{
+		await Product.update(data, {
+			where: {
+				id: request.params.id,
+			}, transaction
+		})
+		// throw Error ('Error')
+		await Product.update(data, {
+				where: {
+					id: parseInt(request.params.id) + 1,
+				}, transaction
+			})
+		await transaction.commit();
+		response.status(200).json({"message": "Both products updated"});
+	} catch (e) {
+		await transaction.rollback();
+		response.json(e);
+	}
+});
+
+
+router.post('/product/:id/stock_update_managed', async(request, response) => {
+	// Assuming Product ID and Product ID + 1 represent the same product type
+	const data = request.body;
+	try{
+		const result = await sequalize.transaction(async (t) => {
+			const product1 = await Product.update(data,{
+				where: {
+					id: request.params.id,
+				},
+				transaction: t
+			});
+			const product2 = await Product.update(data, {
+				where: {
+					id: parseInt(request.params.id) + 1,
+				},
+				transaction: t
+			});
+			// throw new Error();
+			response.status(200).json({"message": "Updated both the products"});
+		});
+	} catch (e) {
+		response.json(e);
+	}
+});
+
+
+router.post('/product/:id/stock_update_with_lock', async(request, response) => {
+	const data = request.body;
+
+	try{
+		const result = await sequalize.transaction(async (t) => {
+			const product = await Product.findOne({
+				where: {
+					id: request.params.id,
+				},
+				lock: t.LOCK.UPDATE,
+				transaction: t
+			});
+			product.stock = data.stock;
+			// wait(1000);
+			await product.save({transaction: t});
+			response.status(200).json({"message": product});
+		});
+	} catch (error){
+		response.json(error);
+	}
+
+});
+
 
 module.exports = router;
